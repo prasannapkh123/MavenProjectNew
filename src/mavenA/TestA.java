c7c1b04b8b7d86a536892e2785868420542eeca4
Avi <Avi@Avi-PC>
2018-04-15 12:59:16 +0530

    first commit

---
+++ src/mavenA/TestA.java
@@ -0,0 +1,14 @@
+package mavenA;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TestA 
+{
+	@Test
+	public void testA()
+	{
+		Reporter.log("hiiiiiiii",true);
+	}
+
+}
